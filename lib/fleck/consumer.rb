397b724c70fba1475ec1d5a9c7ef5b60251b4c34
Groza Sergiu <serioja90@gmail.com>
2016-01-09 00:22:24 +0100

    Split consumer code to different methods and implement restart on failure

--- lib/fleck/consumer.rb
+++ lib/fleck/consumer.rb
@@ -8,19 +8,7 @@ module Fleck
     def self.inherited(subclass)
       super
       init_consumer(subclass)
-
-      # Use TracePoint to autostart the consumer when ready
-      trace = TracePoint.new(:end) do |tp|
-        if tp.self == subclass
-          # disable tracing when we reach the end of the subclass
-          trace.disable
-          # create a new instance of the subclass, in order to start the consumer
-          [subclass.configs[:concurrency].to_i, 1].max.times do |i|
-            subclass.consumers << subclass.new(i)
-          end
-        end
-      end 
-      trace.enable
+      autostart(subclass)
     end
 
     def self.configure(opts = {})
@@ -38,9 +26,24 @@ module Fleck
       subclass.consumers = []
     end
 
+    def self.autostart(subclass)
+      # Use TracePoint to autostart the consumer when ready
+      trace = TracePoint.new(:end) do |tp|
+        if tp.self == subclass
+          # disable tracing when we reach the end of the subclass
+          trace.disable
+          # create a new instance of the subclass, in order to start the consumer
+          [subclass.configs[:concurrency].to_i, 1].max.times do |i|
+            subclass.consumers << subclass.new(i)
+          end
+        end
+      end
+      trace.enable
+    end
+
     def initialize(thread_id)
       @thread_id    = thread_id
-      @delivery_tag = nil
+      @connection   = nil
 
       @host       = configs[:host]
       @port       = configs[:port]
@@ -50,25 +53,10 @@ module Fleck
       @queue_name = configs[:queue]
 
       logger.info "Launching #{self.class.to_s.color(:yellow)} consumer ..."
-      logger.info "Connecting to #{@host}:#{@port}#{@vhost} as #{@user}"
-      @connection = Bunny.new(host: @host, port: @port, user: @user, pass: @pass, vhost: @vhost)
-      @connection.start
 
-      logger.debug "Creating a new channel for #{self.class.to_s.color(:yellow)} consumer"
-      @channel = @connection.create_channel
-      @channel.prefetch(1) # prevent from dispatching a new RabbitMQ message, until the previous message is not processed
-      @queue   = @channel.queue(@queue_name, auto_delete: false)
-
-      logger.debug "Consuming from queue: #{@queue_name.color(:green)}"
-      consumer = self
-      @thread = Thread.new do
-        @subscription = @queue.subscribe(manual_ack: true, block: true) do |delivery_info, metadata, payload|
-          @delivery_tag = delivery_info.delivery_tag
-          on_message(delivery_info, metadata, payload)
-          @channel.ack(@delivery_tag)
-        end
-      end
-      @thread.abort_on_exception = true
+      connect!
+      create_channel!
+      subscribe!
 
       at_exit do
         terminate
@@ -76,12 +64,14 @@ module Fleck
     end
 
     def on_message(delivery_info, metadata, payload)
-      raise NotImplementedError.new("(#{self.class}) You must implement #on_message(delivery_info, metadata, payload) method.")
+      raise NotImplementedError.new("You must implement #on_message(delivery_info, metadata, payload) method")
     end
 
     def terminate
-      @channel.close unless @channel.closed?
-      logger.info "Consumer successfully terminated."
+      unless @channel.closed?
+        @channel.close
+        logger.info "Consumer successfully terminated."
+      end
     end
 
     def logger
@@ -94,6 +84,50 @@ module Fleck
 
     def configs
       @configs ||= self.class.configs
+    end
+
+    protected
+
+    def connect!
+      if @connection && !@connection.closed?
+        logger.info("Closing the opened connection...")
+        @connection.close
+      end
+
+      logger.info "Connecting to #{@host}:#{@port}#{@vhost} as #{@user}"
+      @connection = Bunny.new(host: @host, port: @port, user: @user, pass: @pass, vhost: @vhost)
+      @connection.start
+    end
+
+    def create_channel!
+      if @channel && !@channel.closed?
+        logger.info("Closing the opened channel...")
+        @channel.close
+      end
+
+      logger.debug "Creating a new channel for #{self.class.to_s.color(:yellow)} consumer"
+      @channel = @connection.create_channel
+      @channel.prefetch(1) # prevent from dispatching a new RabbitMQ message, until the previous message is not processed
+      @queue   = @channel.queue(@queue_name, auto_delete: false)
+    end
+
+    def subscribe!
+      logger.debug "Consuming from queue: #{@queue_name.color(:green)}"
+      @subscription = @queue.subscribe(manual_ack: true) do |delivery_info, metadata, payload|
+        begin
+          on_message(delivery_info, metadata, payload)
+          @channel.ack(delivery_info.delivery_tag) unless @channel.closed?
+        rescue StandardError, NotImplementedError => e
+          logger.error e.inspect + "\n" + e.backtrace.join("\n")
+          restart!
+        end
+      end
+    end
+
+    def restart!
+      connect!
+      create_channel!
+      subscribe!
     end
   end
 end
