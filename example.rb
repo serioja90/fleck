c048f3753e635b8caa03425a34121c1fc150f0b0
Groza Sergiu <serioja90@gmail.com>
2016-02-26 16:39:38 +0100

    Update the example.rb

--- example.rb
+++ example.rb
@@ -24,17 +24,22 @@ class First < Fleck::Consumer
   configure queue: "example.queue", concurrency: CONCURRENCY.to_i
 
   def on_message(request, response)
-    if request.action == "incr"
-      response.body = "#{request.params[:num].to_i + 1}. Hello, World!"
+    if rand > 0.1
+      if request.action == "incr"
+        response.body = "#{request.params[:num].to_i + 1}. Hello, World!"
+      else
+        response.not_found
+      end
     else
-      response.not_found
+      logger.warn "REJECTING REQUEST {headers: #{request.headers}, params: #{request.params}"
+      response.reject!(requeue: true)
     end
   end
 end
 
 Thread.new do
   SAMPLES.times do |i|
-    client.request({action: :incr}, {num: i}, true) do |request, response|
+    client.request(headers: {action: :incr}, params: {num: i}, async: true) do |request, response|
       if response.status == 200
         request.logger.debug response.body
       else
