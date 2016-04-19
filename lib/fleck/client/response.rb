2261cabb73c7a1b4eb90184b675a6d5409c7facf
Groza Sergiu <serioja90@gmail.com>
2016-04-19 11:35:20 +0200

    NEW: add deprecation support to Fleck::Client response

--- lib/fleck/client/response.rb
+++ lib/fleck/client/response.rb
@@ -3,13 +3,18 @@ module Fleck
   class Client::Response
     include Fleck::Loggable
 
-    attr_accessor :status, :headers, :body, :errors
+    attr_accessor :status, :headers, :body, :errors, :deprecated
     def initialize(payload)
-      @data    = Oj.load(payload, mode: :compat).to_hash_with_indifferent_access
-      @status  = @data["status"]
-      @headers = @data["headers"] || {}
-      @body    = @data["body"]
-      @errors  = @data["errors"] || []
+      @data       = Oj.load(payload, mode: :compat).to_hash_with_indifferent_access
+      @status     = @data["status"]
+      @headers    = @data["headers"] || {}
+      @body       = @data["body"]
+      @errors     = @data["errors"] || []
+      @deprecated = !!@data["deprecated"]
+    end
+
+    def deprecated?
+      @deprecated
     end
   end
 end
