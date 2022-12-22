67a2e253b91db1273273138d19db2b3f658495a6
Groza Sergiu <groza@opinioni.net>
2022-12-22 17:17:11 +0100

    FIX: move `HashWithIndifferentAccess` class under `Fleck` module, in order to namespace it and prevent conflicts with `ActiveSupport::HashWithIndifferentAccess`

--- lib/fleck/utilities/hash_with_indifferent_access.rb
+++ lib/fleck/utilities/hash_with_indifferent_access.rb
@@ -1,45 +1,47 @@
 # frozen_string_literal: true
 
-# `HashWithIndifferentAccess` extends `Hash` class and adds the possibility to access values
-# by using both string and symbol keys indifferently.
-class HashWithIndifferentAccess < Hash
-  def initialize(original)
-    super(nil)
-    copy_from(original)
-  end
-
-  def []=(key, value)
-    super(key.to_s, self.class.convert_value(value))
-  end
-
-  def [](key)
-    super(key.to_s)
-  end
-
-  def fetch(key, *extras)
-    super(key.to_s, *extras)
-  end
-
-  def delete(key)
-    super(key.to_s)
-  end
-
-  def self.convert_value(value)
-    case value
-    when Hash
-      value.to_hash_with_indifferent_access
-    when Array
-      value.map! { |item| item.is_a?(Hash) || item.is_a?(Array) ? HashWithIndifferentAccess.convert_value(item) : item }
-    else
-      value
+module Fleck
+  # `HashWithIndifferentAccess` extends `Hash` class and adds the possibility to access values
+  # by using both string and symbol keys indifferently.
+  class HashWithIndifferentAccess < Hash
+    def initialize(original)
+      super(nil)
+      copy_from(original)
     end
-  end
 
-  protected
+    def []=(key, value)
+      super(key.to_s, self.class.convert_value(value))
+    end
 
-  def copy_from(original)
-    original.each do |key, value|
-      self[key] = self.class.convert_value(value)
+    def [](key)
+      super(key.to_s)
+    end
+
+    def fetch(key, *extras)
+      super(key.to_s, *extras)
+    end
+
+    def delete(key)
+      super(key.to_s)
+    end
+
+    def self.convert_value(value)
+      case value
+      when Hash
+        value.to_hash_with_indifferent_access
+      when Array
+        value.map! { |item| item.is_a?(Hash) || item.is_a?(Array) ? Fleck::HashWithIndifferentAccess.convert_value(item) : item }
+      else
+        value
+      end
+    end
+
+    protected
+
+    def copy_from(original)
+      original.each do |key, value|
+        self[key] = self.class.convert_value(value)
+      end
     end
   end
 end
@@ -47,7 +49,7 @@ end
 # Open `Hash` class to add `#to_hash_with_indifferent_access` method and some filter features.
 class Hash
   def to_hash_with_indifferent_access
-    HashWithIndifferentAccess.new(self)
+    Fleck::HashWithIndifferentAccess.new(self)
   end
 
   def to_s
